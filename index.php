e4fff54726ade9361c6400247487d5c65ea34149
sSsaAa3 <103163095+sSsaAa3@users.noreply.github.com>
2022-06-08 13:07:11 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head><title> Saludos desde php </title></head>
+
+<body bgcolor="#45B39D">
+<Font color="#A569BD "> Se comienza a ejecutar la instruccion php </font> <br><br>
+ 
+<p>
+
+<?php
+$Tuname=$_POST['nombre']
+echo ("Holaaa Persona del planeta tierra llamada $Tuname ;)");
+
+?> </p>
+<p>
+  PHP es en parte un enfoque concreto para la implementación de tecnología y en parte un concepto utilizado para explicar la relación entre sistemas de sobremesa inteligentes y sistemas multiusuario tradicionales, se ha convertido en un término global utilizado para describir las estrategias </p>
+
+<p>
+
+Esto permite la visualización en cualquier navegador con esta estructura, se
+permite la programación gratuita. </p>
+
+<p>
+Sino contamos con un -servidor- no podemos ver Funcionando el Código PHP. </p>
+
+</body>
+</html>
